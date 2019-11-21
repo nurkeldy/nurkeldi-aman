24525ed2c159bc2e02f261271bd3bdb72df97918
nurkeldy <58022967+nurkeldy@users.noreply.github.com>
2019-11-21 13:19:15 +0600

    Add files via upload

---
+++ TASK31.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int k = 647 * 170;
+	int num;
+	num = k / (30 * 30);
+	cout << num;
+}
+#32
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	a = 10 * (237 - 7) / 10 + 7;
+}
