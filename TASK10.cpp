a6be678a09c29e8521f9f0fec68d12bd488a6e5c
nurkeldy <58022967+nurkeldy@users.noreply.github.com>
2019-11-21 12:55:23 +0600

    Add files via upload

---
+++ TASK10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	double x = 1.7;
+	double r = pow((x + 1), 2) + 3 * (x + 1);
+	cout << r << endl;
+	int x1 = 3;
+	int r1 = pow((x1 + 1), 2) + 3 * (x1 + 1);
+	cout << r1 << endl;
+	return 0;
+}
